c6ce113541f96ed289d66d3c38b52ae4d4bc8baa
Jason Steen <jason.steen@monash.edu>
2018-02-26 13:42:42 +1100

    syntax errors

--- src/pipeline.py
+++ src/pipeline.py
@@ -51,7 +51,7 @@ def make_pipeline(state):
         input=output_from('align_bwa', 'original_fastqs'),
         filter=formatter('processed_fastqs/(?P<sample>[a-zA-Z0-9_-]+.bam'),
         add_inputs=add_inputs(['.+/{sample[0]}_I2.fastq.gz'])
-        output='alignments/{sample[0]}.locatit.bam'
+        output='alignments/{sample[0]}.locatit.bam')
     
     # index bam file
     pipeline.transform(
