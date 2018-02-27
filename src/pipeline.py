f6de894f0ef85672f037e04c58a316fa45d122a6
Jason Steen <jason.steen@monash.edu>
2018-02-27 13:25:57 +1100

    trying to work out Index addition for locatit

--- src/pipeline.py
+++ src/pipeline.py
@@ -51,7 +51,7 @@ def make_pipeline(state):
         name='run_locatit',
         input=output_from('align_bwa', 'original_fastqs'),
         filter=formatter('alignments/(?P<sample>[a-zA-Z0-9_-]+).bam'),
-        add_inputs=add_inputs(['{path[0]}/{sample[0]}_I2.fastq.gz']),
+        add_inputs=add_inputs(['{sample[0]}_I2.fastq.gz']),
         output='alignments/{sample[0]}.locatit.bam')
     
     # index bam file
