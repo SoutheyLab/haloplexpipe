8ecec9fa4005971806707bdf690dda29140af2f8
Jason Steen <jason.steen@monash.edu>
2018-02-27 15:53:36 +1100

    f

--- src/pipeline.py
+++ src/pipeline.py
@@ -50,9 +50,9 @@ def make_pipeline(state):
         name='run_locatit',
         input=output_from('align_bwa', 'original_fastqs'),
         #filter=formatter('alignments/(?P<sample>[a-zA-Z0-9_-]+).bam'),
-        filter=regex(r'.+([a-zA-Z0-9_-]+).bam'),
+        filter=regex(r'.+/([a-zA-Z0-9_-]+).bam'),
         add_inputs=add_inputs(r'.+/\1_I2.fastq.gz'),
-        output='alignments/{sample[0]}.locatit.bam')
+        output=r'alignments/\1.locatit.bam')
     
 #    filter=regex(r'.+/(.+BS\d{4,6}.+S\d+)\..+\.txt'),
 #        output=r'all_sample.summary.\1.txt',
