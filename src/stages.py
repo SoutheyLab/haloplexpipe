89c4b33389fe96ac66cc800f5635a0159f7ada5d
Jason Steen <jason.steen@monash.edu>
2018-02-27 13:39:08 +1100

    stage modification

--- src/stages.py
+++ src/stages.py
@@ -105,7 +105,7 @@ class Stages(object):
         run_stage(self.state, 'align_bwa', command)
 
     def run_locatit(self, inputs, bam_out,):
-        bam_in, index_file = inputs 
+        bam_in, [index_file] = inputs 
         command = 'java -Xmx{mem}G -jar /home/jste0021/vh83/local_software/agent/LocatIt_v4.0.1.jar -q 25 -m 1 -U -IB -OB -b {locatit_bed_file} ' \
                    '-o {bam_out} {bam_in} {index_file}' \
                  .format(mem=self.state.config.get_stage_options('run_locatit', 'mem'),
