d0caf0cec26f6433e329ad406d52fd6a54c0c478
Jason Steen <jason.steen@monash.edu>
2018-02-28 08:12:06 +1100

    fixed typo

--- src/pipeline.py
+++ src/pipeline.py
@@ -52,8 +52,8 @@ def make_pipeline(state):
         #filter=formatter('alignments/(?P<sample>[a-zA-Z0-9_-]+).bam'),
         filter=regex(r'.+/([a-zA-Z0-9_-]+).+'),
         #add_inputs=add_inputs(r'.+/\1_I2.fastq.gz'),
-        output=r'alignments/\1.locatit.bam')
-        extras="\1"
+        output=r'alignments/\1.locatit.bam',
+        extras="\1")
 
 #    filter=regex(r'.+/(.+BS\d{4,6}.+S\d+)\..+\.txt'),
 #        output=r'all_sample.summary.\1.txt',
